ecf8e9d5c21793169ca7edfca6f9aa6377c3178d
G-coder <gregoire.woroniak@gmail.com>
2025-01-29 12:58:54 +0100

    yolo training for tape detection

--- tape_detection/train.py
+++ tape_detection/train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 # Load pre-trained model
-model = YOLO("yolo11s.pt") 
+model = YOLO("yolo11m.pt") 
 
-results = model.train(data="./dataset/data.yaml", epochs=100, imgsz=640)
+results = model.train(data="./data.yaml", epochs=100, imgsz=640)
